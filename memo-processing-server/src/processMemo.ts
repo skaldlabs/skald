0fced9a07cdfd79015584fac6472a237c6d0223c
yakkomajuri <yakko.majuri@gmail.com>
2025-10-10 13:04:22 -0300

    feat: remove knowledge base update agent from hot path for now

--- memo-processing-server/src/processMemo.ts
+++ memo-processing-server/src/processMemo.ts
@@ -1,42 +1,22 @@
 import { KnowledgeBaseUpdateAction, createKnowledgeBaseUpdateAgent } from "./agents/knowledgeBaseUpdateAgent/knowledgeBaseUpdateAgent"
-import { deleteMemo, fetchMemo, FetchMemoResult } from "./db/memo"
+import { createMemoChunks, extractTagsFromMemo, generateMemoSummary } from "./agents/knowledgeBaseUpdateAgent/memoOperations"
+import { deleteMemo, fetchMemo, FetchMemoResult, updateMemo } from "./db/memo"
 
 
 export const processMemo = async (memoUuid: string) => {
-    const startTime = Date.now()
     const memo = await fetchMemo(memoUuid)
+    await updateMemo(memo.uuid, [{
+        column: 'pending',
+        value: false
+    }])
+    const promises = [
+        createMemoChunks(memo.uuid, memo.content),
+        extractTagsFromMemo(memo),
+        generateMemoSummary(memo)
+    ]
 
-    console.log(`Time taken to process memo: ${Date.now() - startTime}ms`)
-
-    const knowledgeBaseUpdateStartTime = Date.now()
-
-    const actions = await _knowledgeBaseUpdate(memo)
-    console.log(actions)
-    for (const action of actions) {
-        // if (action.action === 'INSERT' && action.content === 'provided_content_unchanged') {
-        //     await updateMemo(memo.uuid, [{
-        //         column: 'pending',
-        //         value: false
-        //     }])
-        //     const promises = [
-        //         _createMemoChunks(memo.uuid, memo.content),
-        //         _extractTagsFromMemo(memo),
-        //         _generateMemoSummary(memo)
-        //     ]
-
-        //     await Promise.all(promises)
-        // }
-    }
-    const knowledgeBaseUpdateEndTime = Date.now()
-    console.log(`Time taken to update knowledge base: ${knowledgeBaseUpdateEndTime - knowledgeBaseUpdateStartTime}ms`)
-    const endTime = Date.now()
-    console.log(`Total time taken to process memo: ${endTime - startTime}ms`)
-
-    // if there's an INSERT action, create the memo chunks and set pending to False
+    await Promise.all(promises)
 }
-
-
-
 
 
 const _knowledgeBaseUpdate = async (memo: FetchMemoResult): Promise<KnowledgeBaseUpdateAction[]> => {
