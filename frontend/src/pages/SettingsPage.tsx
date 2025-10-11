770de18e9df7fd2d25be69db7db2d71d068ae166
yakkomajuri <yakko.majuri@gmail.com>
2025-10-11 20:51:50 -0300

    feat: padding for dash

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -3,7 +3,7 @@ import { AppLayout } from '@/components/AppLayout/AppLayout'
 export const SettingsPage = () => {
     return (
         <AppLayout>
-            <div className="p-6">
+            <div>
                 <p>Your application settings will be displayed here.</p>
             </div>
         </AppLayout>
