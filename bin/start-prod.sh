3b1e647c13ef2761973ce0fb7163f3ec3aa67f6b
yakkomajuri <yakko.majuri@gmail.com>
2025-10-26 20:37:37 -0300

    chore: update deploy api config

--- bin/start-prod.sh
+++ bin/start-prod.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-gunicorn -k gevent --workers=2 --bind 0.0.0.0:8000 skald.wsgi --log-level debug --timeout 45
+gunicorn --workers=2 --bind 0.0.0.0:8000 skald.wsgi --log-level debug --timeout 45
