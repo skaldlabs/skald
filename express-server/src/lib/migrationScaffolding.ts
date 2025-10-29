1c1ecbeb93d10158f8b971722ca3516da28f819e
yakkomajuri <yakko.majuri@gmail.com>
2025-10-29 13:10:57 -0300

    chore: remove old migration script

--- express-server/src/lib/migrationScaffolding.ts
+++
@@ -1,9 +0,0 @@
-export class MigrationOperation {
-    SQL: string
-    rollbackSQL: string
-
-    constructor({ SQL, rollbackSQL }: { SQL: string; rollbackSQL: string }) {
-        this.SQL = SQL
-        this.rollbackSQL = rollbackSQL
-    }
-}
