477601f4a80ec0a7bd2a857566f47f4ddf734feb
pedrique <pedrique@gmail.com>
2025-11-01 18:34:03 -0300

    chore: delete memo-processing-server

--- memo-processing-server/src/sentry.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/node'
-import { DEBUG, SENTRY_DSN } from './settings'
-
-if (!DEBUG && SENTRY_DSN) {
-    Sentry.init({
-        dsn: SENTRY_DSN,
-        sendDefaultPii: true,
-    })
-}
