389a005097302aa24086e70938d4b6b367e2c3a1
pedrique <pedrique@gmail.com>
2025-11-01 18:33:46 -0300

    chore: delete python server

--- skald/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from skald.services.subscription_service import SubscriptionService
-from skald.services.usage_tracking_service import UsageTrackingService
-
-__all__ = ["SubscriptionService", "UsageTrackingService"]
