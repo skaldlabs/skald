ad01d22deff0b8ff866a9a2120b36886143dff41
pedrique <pedrique@gmail.com>
2025-11-03 00:05:51 -0300

    fix: avoid capture posthog event in test

--- backend/src/lib/posthogUtils.ts
+++ backend/src/lib/posthogUtils.ts
@@ -1,10 +1,13 @@
-import { IS_SELF_HOSTED_DEPLOY, POSTHOG_API_KEY, POSTHOG_HOST } from '@/settings'
+import { IS_SELF_HOSTED_DEPLOY, POSTHOG_API_KEY, POSTHOG_HOST, TEST } from '@/settings'
 import { PostHog } from 'posthog-node'
 
 export const posthog = new PostHog(POSTHOG_API_KEY, { host: POSTHOG_HOST })
 
 export function posthogCapture(event_name: string, distinct_id: string, properties: Record<string, unknown>): void {
     if (IS_SELF_HOSTED_DEPLOY && !['organization_created'].includes(event_name)) {
+        return
+    }
+    if (TEST) {
         return
     }
 
