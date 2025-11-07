53d4f955d3c9c7b95d6e88cdfc911909991f57ac
yakkomajuri <yakko.majuri@gmail.com>
2025-11-07 11:30:07 -0300

    fix: fix s3 utils to pull correct settings

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -85,10 +85,9 @@ export const RABBITMQ_VHOST = process.env.RABBITMQ_VHOST || '/'
 export const RABBITMQ_QUEUE_NAME = process.env.RABBITMQ_QUEUE_NAME || 'process_memo'
 
 export const AWS_REGION = process.env.AWS_REGION || 'us-east-2'
-
-// must be set but aren't explicitly read -- aws cli reads them from the environment
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME
 
 // ---- LLM Configuration ----
 export const LLM_PROVIDER = process.env.LLM_PROVIDER || 'openai'
