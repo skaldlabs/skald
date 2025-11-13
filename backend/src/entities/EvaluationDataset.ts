7c05f95847d2c050e3e1cf971361762201ab9460
yakkomajuri <yakko.majuri@gmail.com>
2025-11-13 18:16:23 -0300

    feat: add experiment entities

---
+++ backend/src/entities/EvaluationDataset.ts
@@ -0,0 +1,25 @@
+import { DeferMode, Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core'
+import { Project } from '@/entities/Project'
+
+@Entity({ tableName: 'skald_evaluationdataset' })
+export class EvaluationDataset {
+    @PrimaryKey({ type: 'uuid' })
+    uuid!: string
+
+    @Property()
+    name!: string
+
+    @Property()
+    description!: string
+
+    @ManyToOne({
+        entity: () => Project,
+        fieldName: 'project_id',
+        deferMode: DeferMode.INITIALLY_DEFERRED,
+        index: 'skald_evaluationdataset_project_id_idx',
+    })
+    project!: Project
+
+    @Property()
+    created_at!: Date
+}
---
+++ backend/src/entities/Experiment.ts
@@ -0,0 +1,37 @@
+import { DeferMode, Entity, ManyToOne, PrimaryKey, Property } from '@mikro-orm/core'
+import { Project } from '@/entities/Project'
+import { EvaluationDataset } from '@/entities/EvaluationDataset'
+
+@Entity({ tableName: 'skald_experiment' })
+export class Experiment {
+    @PrimaryKey({ type: 'uuid' })
+    uuid!: string
+
+    @Property()
+    title!: string
+
+    @Property()
+    description!: string
+
+    @Property()
+    properties!: Record<string, any>
+
+    @ManyToOne({
+        entity: () => Project,
+        fieldName: 'project_id',
+        deferMode: DeferMode.INITIALLY_DEFERRED,
+        index: 'skald_experiment_project_id_idx',
+    })
+    project!: Project
+
+    @ManyToOne({
+        entity: () => EvaluationDataset,
+        fieldName: 'evaluation_dataset_id',
+        deferMode: DeferMode.INITIALLY_DEFERRED,
+        index: 'skald_experiment_evaluation_dataset_id_idx',
+    })
+    evaluationDataset!: EvaluationDataset
+
+    @Property()
+    created_at!: Date
+}
