7b1ed5e563a1df74269509fa338872ae5c0549b9
yakkomajuri <yakko.majuri@gmail.com>
2025-11-24 08:37:59 -0300

    chore: run prettier

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -8,13 +8,13 @@ const logger = pino({
     transport:
         process.env.NODE_ENV === 'development'
             ? {
-                target: 'pino-pretty',
-                options: {
-                    colorize: true,
-                    translateTime: 'HH:MM:ss Z',
-                    ignore: 'pid,hostname',
-                },
-            }
+                  target: 'pino-pretty',
+                  options: {
+                      colorize: true,
+                      translateTime: 'HH:MM:ss Z',
+                      ignore: 'pid,hostname',
+                  },
+              }
             : undefined,
 })
 
