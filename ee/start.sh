52feff9bfc15977025646cdba38091663abc92ec
yakkomajuri <yakko.majuri@gmail.com>
2026-01-13 20:55:07 -0300

    feat: ee build

---
+++ ee/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Running migrations..."
+cd /app/backend && pnpm run migration:up
+
+echo "Starting enterprise server..."
+cd /app/ee && exec node dist/src/enterpriseServer.js
