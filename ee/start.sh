c2385cd66f16c07a97cb34093a4976c625c94866
yakkomajuri <yakko.majuri@gmail.com>
2026-01-13 21:26:32 -0300

    fix: more fixes

--- ee/start.sh
+++ ee/start.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 cd /app/backend && pnpm run migration:up
 
 echo "Starting enterprise server..."
-cd /app/ee && NODE_PATH=/app/backend/node_modules:/app/ee/node_modules exec node dist/ee/src/enterpriseServer.js
+cd /app/ee && NODE_PATH=/app/backend/node_modules:/app/ee/node_modules exec node dist/ee/src/index.js
